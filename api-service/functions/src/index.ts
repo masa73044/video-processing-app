16cf32d6940cab58118a5f90a4301fe84fb8ccd3
masa73044 <108378047+masa73044@users.noreply.github.com>
2023-08-03 00:20:15 +0700

    gcloud storage

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -10,20 +10,19 @@
 // import { onRequest } from "firebase-functions/v2/https";
 import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
-import { initializeApp } from "firebase-admin/app";
-import { Firestore } from "firebase-admin/firestore";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
 
 initializeApp();
 
 const fireStore = new Firestore();
+const storage = new Storage()
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", { structuredData: true });
-//   response.send("Hello from Firebase!");
-// });
+const rawVideoBucketName = "mk2-yt-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -38,3 +37,33 @@ export const createUser = functions.auth.user().onCreate((user) => {
 
   return;
 });
+
+
+export const generateUploadUrl = onCall(
+ {maxInstances: 1}, async (request) => {
+
+  // Check if user is authenticated 
+
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function mus be called while authenticated."
+    )
+  }
+
+  const auth = request.auth
+  const data = request.data
+  const bucket = storage.bucket(rawVideoBucketName)
+
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
+ }
+)
