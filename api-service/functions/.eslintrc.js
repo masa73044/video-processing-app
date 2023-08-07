5219cf6d5554951d97896180c68ce2252e1fb287
masa73044 <108378047+masa73044@users.noreply.github.com>
2023-08-07 13:26:00 +0700

    completed add video function

--- api-service/functions/.eslintrc.js
+++ api-service/functions/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
+    "src/index.ts"
   ],
   plugins: [
     "@typescript-eslint",
@@ -28,5 +29,6 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "comma-dangle": "off",
   },
 };
--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -18,11 +18,22 @@ import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
-const fireStore = new Firestore();
-const storage = new Storage()
+const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "mk3-yt-raw-videos";
+
+const videoCollectionId = "videos"
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
 
 
-const rawVideoBucketName = "mk2-yt-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -31,7 +42,7 @@ export const createUser = functions.auth.user().onCreate((user) => {
     photoUrl: user.photoURL,
   };
 
-  fireStore.collection("users").doc(user.uid).set(userInfo);
+  firestore.collection("users").doc(user.uid).set(userInfo);
 
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
 
@@ -39,22 +50,20 @@ export const createUser = functions.auth.user().onCreate((user) => {
 });
 
 
-export const generateUploadUrl = onCall(
- {maxInstances: 1}, async (request) => {
-
-  // Check if user is authenticated 
-
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authentication
   if (!request.auth) {
     throw new functions.https.HttpsError(
       "failed-precondition",
-      "The function mus be called while authenticated."
-    )
+      "The function must be called while authenticated."
+    );
   }
 
-  const auth = request.auth
-  const data = request.data
-  const bucket = storage.bucket(rawVideoBucketName)
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
 
+  // Generate a unique filename for upload
   const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
   // Get a v4 signed URL for uploading file
@@ -65,5 +74,10 @@ export const generateUploadUrl = onCall(
   });
 
   return {url, fileName};
- }
-)
+});
+
+export const getVideos = onCall({maxInstances: 1}, async() => {
+  const snapshot = await firestore.collection(videoCollectionId).limit(10).get()
+  return snapshot.docs.map((doc) => doc.data())
+
+})
