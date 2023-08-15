b7907c725114f72c2901d771ed349314075697f5
masa73044 <108378047+masa73044@users.noreply.github.com>
2023-08-15 10:17:34 +0700

    Changed layout, hero bg

--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -2,7 +2,13 @@ import styles from "./page.module.css";
 import { getVideos } from "./firebase/functions";
 import Link from "next/link";
 import Image from "next/image";
-import Test from "./new-app/page";
+import "./assets/vendor/aos/aos.css";
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
+import "./assets/vendor/glightbox/css/glightbox.min.css";
+import "./assets/vendor/swiper/swiper-bundle.min.css";
+import "./assets/css/style.css";
 
 export default async function Home() {
   const videos = await getVideos();
